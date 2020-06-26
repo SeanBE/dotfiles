4b57a528454beab1cb5f5bebe6f11927fcd96882
seanBE <SeanBE@users.noreply.github.com>
2020-06-26 17:54:46 +0200

    configure zsh prompt via multiline

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -4,5 +4,8 @@
 # Originally from https://github.com/ohmyzsh/ohmyzsh/blob/master/themes/Soliah.zsh-theme
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[white]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
-PROMPT='%{$fg[blue]%}%n%{$reset_color%}@%{$fg[red]%}%M%{$reset_color%}: %{$fg[blue]%}%~%b%{$reset_color%} $(git_prompt_info)
+PROMPT='%{$fg[blue]%}%n%{$reset_color%}'
+PROMPT+='@'
+PROMPT+='%{$fg[red]%}%M%{$reset_color%}: %{$fg[blue]%}%~%b%{$reset_color%} $(git_prompt_info)'
+PROMPT+='
 $ '
