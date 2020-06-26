a8a33bfa5b630324609cce1a83106b964d016a10
seanBE <SeanBE@users.noreply.github.com>
2020-06-26 17:02:09 +0200

    add go and rust install steps

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,7 @@
 sudo dnf -y update
 
 sudo dnf copr enable pschyska/alacritty
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm
 sudo dnf -y install zsh exa vim vim-enhanced vim-X11 alacritty
 
 sudo dnf -y install sway swayidle swaylock waybar rofi \
@@ -12,4 +13,13 @@ sudo dnf -y install make lapack-devel git-delta blas-devel \
   ShellCheck gcc-c++ pkgconf gcc docker jq openssl-devel ansible
 
 sudo dnf -y install weechat vlc ripgrep nmap htop ffmpeg \
-  rclone zathura ftp acpi fuse exfat-utils
+    rclone zathura ftp acpi fuse exfat-utils 
+
+# install some languages
+wget -O /tmp/go.tar.gz https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# misc
+mkdir -m 700 "$HOME/.ssh"
