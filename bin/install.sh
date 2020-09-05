4362c9ad3a355f0a8ba3a0d66264d267a4c89248
seanBE <SeanBE@users.noreply.github.com>
2020-09-05 12:02:50 +0200

    update bin install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 set -e
+
+# TODO: run sudo with script using zsh
+
+# system depdencies
 sudo dnf -y update
-
-sudo dnf copr enable pschyska/alacritty
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm
-
+sudo dnf -y copr enable pschyska/alacritty
 sudo dnf -y install \
+	https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm \
+	https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
     zsh \
     exa \
     vim \
@@ -45,7 +48,7 @@ sudo dnf -y install \
     nmap \
     htop \
     ffmpeg \
-    rclone \
+    rclone bat \
     postgresql \
     zlib-devel \
     zathura \
@@ -64,19 +67,30 @@ sudo dnf -y install \
     chromium \
     docker-compose \
     libpq-devel \
-    pandoc texlive-scheme-full 
+    pandoc texlive-scheme-full \
+	youtube-dl mpv sxiv xv-devel
 
-# TODO: clean this part up.
-# install some languages
-wget -O /tmp/go.tar.gz https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+# python3-docutils
 
+# Set zsh as the default shell
+chsh -s /bin/zsh
+
+# Install some languages
+## Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 cargo install git-absorb
+cargo install xsv
 cargo install --locked --git https://github.com/dandavison/delta
-go get github.com/yory8/clipman
 
+## Golang
+wget -O /tmp/go.tar.gz https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+
+go get github.com/yory8/clipman
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.27.0
+
+## build i3Blocks
 git clone https://github.com/vivien/i3blocks
 cd i3blocks
 ./autogen.sh
@@ -85,14 +99,9 @@ make
 sudo make install
 
 git clone https://github.com/vivien/i3blocks-contrib.git ~/.config/i3blocks/blocklets
+cd ~/.config/i3blocks/blocklets/cpu_usage2 && make
 
-cd ~/.config/i3blocks/blocklets/cpu_usage2 
-make
-
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
-
+# Misc
 sudo usermod -aG docker "$USER" # satisfy docker post linux reqs.
 sudo systemctl enable docker
-
-# misc
 mkdir -m 700 "$HOME/.ssh"
