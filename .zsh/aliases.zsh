182d6857267ffa0674830e78dbc26123620966fb
seanBE <SeanBE@users.noreply.github.com>
2020-09-26 10:41:26 +0100

    add hledger aliaes

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,7 +10,12 @@ alias pip='python -m pip'
 alias ssh='TERM=xterm ssh'
 alias za='zathura --fork'
 alias explore='fzf --preview "bat {-1} --color=always"'
-alias hledger="docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/full-fledged-hledger:z dastapov/full-fledged-hledger:latest"
+
+hledger_image="dastapov/full-fledged-hledger:latest"
+run_hledger_in_docker="docker run --rm -it --user $(id -u):$(id -g) -v $HOME/.ledger:/full-fledged-hledger:z"
+alias hledger="$run_hledger_in_docker $hledger_image hledger -f all.journal"
+alias hledger-web="$run_hledger_in_docker -p '127.0.0.1:5000:5000' $hledger_image hledger-web -f all.journal --serve --host 0.0.0.0"
+alias hledger-build="$run_hledger_in_docker $hledger_image ./export.sh --rebuild --trace"
 
 # https://www.topbug.net/blog/2016/10/11/speed-test-check-the-existence-of-a-command-in-bash-and-zsh/
 if (( $+commands[exa] )); then
