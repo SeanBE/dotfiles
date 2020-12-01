913afa0f0f7094d4d3bfb819bff19144c8b0408a
seanBE <SeanBE@users.noreply.github.com>
2020-12-01 10:06:27 +0000

    drop virtualenv zinit plugin and pin poetry to 1.1.4

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -26,6 +26,9 @@ zinit lucid for \
     OMZP::ssh-agent/ssh-agent.plugin.zsh \
     "https://github.com/agkozak/zsh-z/blob/master/zsh-z.plugin.zsh"
 
+zinit light zinit-zsh/z-a-bin-gem-node
+zinit light zinit-zsh/z-a-patch-dl
+
 zinit ice as"completion"
 zinit snippet 'https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker'
 
@@ -54,21 +57,17 @@ zinit light lukechilds/zsh-nvm
 ## https://github.com/nvm-sh/nvm/issues/1277#issuecomment-536218082
 export PATH="$NVM_DIR/versions/node/v$(<$NVM_DIR/alias/default)/bin:$PATH"
 
-# or PYENV_ROOT="${HOME}/.pyenv"
-zinit ice atclone'./libexec/pyenv init - > zpyenv.zsh' \
+zinit ice atclone"git clone https://github.com/pyenv/pyenv-virtualenv.git $PWD/plugins/pyenv-virtualenv; \
+    ./libexec/pyenv init - > zpyenv.zsh" \
     atinit'export PYENV_ROOT="$PWD"' atpull"%atclone" \
     as"program" pick"bin/pyenv" src"zpyenv.zsh" nocompile"!"
 zinit light pyenv/pyenv
 
-zinit ice cloneonly nocompile nocompletions \
-    atclone"mkdir -p $PYENV_ROOT/plugins;
-    ln -sf ${ZINIT[PLUGINS_DIR]}/pyenv---pyenv-virtualenv $PYENV_ROOT/plugins/pyenv-virtualenv"
-zinit light pyenv/pyenv-virtualenv
-
-zplugin ice as"completion" atclone"python ./get-poetry.py; \
-    $HOME/.poetry/bin/poetry completions zsh > _poetry" \
-    atpull"%atclone" atload'PATH+=:$HOME/.poetry/bin'
-zplugin light python-poetry/poetry
+# keep an eye on autocomplete template bug (mean time set opts to local)
+zinit ice as"completion" pick"_poetry" atpull"%atclone" \
+    atload'PATH+=:$HOME/.poetry/bin' cloneopts'--depth 1 --branch 1.1.4' \
+    atclone"python ./get-poetry.py; $HOME/.poetry/bin/poetry completions zsh >! _poetry"
+zinit light python-poetry/poetry
 
 zinit light zsh-users/zsh-history-substring-search
 
