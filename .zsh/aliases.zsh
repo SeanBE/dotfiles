8926b7e1d41ab8342420150fb5021e2724209d08
seanBE <SeanBE@users.noreply.github.com>
2021-01-18 20:01:21 +0000

    alias and tsserver update

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -13,7 +13,7 @@ alias explore='fzf --preview "bat {-1} --color=always"'
 
 hledger_image="dastapov/full-fledged-hledger:latest"
 run_hledger_in_docker="docker run --rm -it --user $(id -u):$(id -g) -v $HOME/.ledger:/full-fledged-hledger:z"
-alias hledger="$run_hledger_in_docker $hledger_image hledger -f all.journal"
+alias hledger="$run_hledger_in_docker $hledger_image hledger -f exchange_rates.journal"
 alias hledger-web="$run_hledger_in_docker -p '127.0.0.1:5000:5000' $hledger_image hledger-web -f all.journal --serve --host 0.0.0.0"
 alias hledger-build="$run_hledger_in_docker $hledger_image ./export.sh --rebuild --trace"
 
