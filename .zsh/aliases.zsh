66e6f2ae84c6b7d360a71bec06176e832f8b392d
seanBE <SeanBE@users.noreply.github.com>
2021-07-14 19:22:11 +0100

    update zsh aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,10 +6,13 @@ alias vim='vimx'
 alias dokcer='docker'
 alias docker-rmi-dangling='docker rmi $* $(docker images -q -f dangling=true)'
 
+alias wget="wget2"
 alias pip='python -m pip'
 alias ssh='TERM=xterm ssh'
 alias za='zathura --fork'
 alias explore='fzf --preview "bat {-1} --color=always"'
+
+alias mutt='TERM=screen-256color neomutt'
 
 hledger_image="dastapov/full-fledged-hledger:latest"
 run_hledger_in_docker="docker run --rm -it --user $(id -u):$(id -g) -v $HOME/.ledger:/full-fledged-hledger:z"
@@ -22,13 +25,4 @@ if (( $+commands[exa] )); then
   alias lss='/bin/ls'
   alias ls='exa'
   alias la='exa -la --sort newest'
-fi
-
-if (( $+commands[htop] )); then
-  alias top='htop'
-fi
-
-
-if (( $+commands[fd] )); then
-  alias find='fd --color=auto --hidden'
 fi
