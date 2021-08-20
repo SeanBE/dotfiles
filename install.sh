0672ca49df480d9047ccbb85e71534e6e4dd49c3
seanBE <SeanBE@users.noreply.github.com>
2021-08-20 21:45:26 +0100

    stop install script on error

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
+set -e
+
 sudo dnf update -y && sudo dnf install -y git ansible stow
 
 git clone https://github.com/SeanBE/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 git submodule update --init
+
+# remove default files to unblock stow
+rm  ~/.bashrc
 
 for f in *; do
     if [[ -d "$f" && "$f" != "ansible" ]]; then
