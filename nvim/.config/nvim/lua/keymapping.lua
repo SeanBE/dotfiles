0a33b0b38cd349f66589c34c419aba35cc71ad28
seanBE <SeanBE@users.noreply.github.com>
2021-08-23 18:59:08 +0100

    drop port from socket controlpath and update telescope file keymapping

--- nvim/.config/nvim/lua/keymapping.lua
+++ nvim/.config/nvim/lua/keymapping.lua
@@ -2,10 +2,10 @@ local map = vim.api.nvim_set_keymap
 
 map('n', '<leader>s', ':w<cr>', {noremap = true, silent = true})
 
-map('i', '<Tab>', 'v:lua.tab_complete()', {expr = true})
-map('s', '<Tab>', 'v:lua.tab_complete()', {expr = true})
-map('i', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
-map('s', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
+--map('i', '<Tab>', 'v:lua.tab_complete()', {expr = true})
+--map('s', '<Tab>', 'v:lua.tab_complete()', {expr = true})
+--map('i', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
+--map('s', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
 
 map('i', 'kk', '<Esc>', {noremap = true})
 map('', '<up>', '<nop>', {noremap = true})
@@ -14,10 +14,11 @@ map('', '<left>', '<nop>', {noremap = true})
 map('', '<right>', '<nop>', {noremap = true})
 map('n', '<leader>c', ':nohl<CR>', {noremap = true, silent = true})
 
+map('n', '<Leader>ff', ":lua require'telescope.builtin'.find_files{}<CR>", { noremap = true, silent = true })
+
 map('n', '<Leader>pp', ":lua require'telescope.builtin'.builtin{}<CR>", { noremap = true, silent = true })
 map('n', '<Leader>rg', ":lua require'telescope.builtin'.live_grep{}<CR>", { noremap = true, silent = true })
 map('n', ';', ":lua require'telescope.builtin'.buffers{}<CR>", { noremap = true, silent = true })
-map('n', '<Leader>bfs', ":lua require'telescope.builtin'.find_files{}<CR>", { noremap = true, silent = true })
 
 map('v', 'J', ":m '>+1<CR>gv=gv", {noremap = true})
 map('v', 'K', ":m '<-2<CR>gv=gv", {noremap = true})
