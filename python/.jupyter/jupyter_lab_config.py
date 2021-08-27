14a0a2e5ce4697f9577b6d2c7e0254efb89e98d4
seanBE <SeanBE@users.noreply.github.com>
2021-08-27 16:51:12 +0100

    fix jupyterlab configuration

---
+++ python/.jupyter/jupyter_lab_config.py
@@ -0,0 +1,16 @@
+c.ExtensionApp.open_browser = False
+
+c.LabServerApp.open_browser = False
+c.LabApp.open_browser = False
+
+c.ServerApp.allow_password_change = False
+c.ServerApp.allow_remote_access = False
+c.ServerApp.allow_root = False
+
+c.ServerApp.ip = '127.0.0.1'
+c.ServerApp.local_hostnames = ['localhost']
+
+c.ServerApp.open_browser = False
+c.ServerApp.password_required = True
+c.ServerApp.port = 8889
+c.ServerApp.webbrowser_open_new = 2
